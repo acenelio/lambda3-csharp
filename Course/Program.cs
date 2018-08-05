7b9755cc39843c4fe71ba29a3ef7a6dd0c7c53c4
Nelio Alves <acenelio@gmail.com>
2018-08-05 10:13:58 -0300

    Variable to lambda expression

--- Course/Program.cs
+++ Course/Program.cs
@@ -13,16 +13,12 @@ namespace Course {
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Action<Product> act = UpdatePrice;
+            Action<Product> act = p => { p.Price += p.Price * 0.1; };
 
             list.ForEach(act);
             foreach (Product p in list) {
                 Console.WriteLine(p);
             }
-        }
-
-        static void UpdatePrice(Product p) {
-            p.Price += p.Price * 0.1;
         }
     }
 }
