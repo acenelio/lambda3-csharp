ca445c9f62c6b15cae3e15cc1430cef2b2897fcd
Nelio Alves <acenelio@gmail.com>
2018-08-05 10:12:17 -0300

    Reference method

---
+++ Course/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Course.Entities;
+using System.Collections.Generic;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(UpdatePrice);
+            foreach (Product p in list) {
+                Console.WriteLine(p);
+            }
+        }
+
+        static void UpdatePrice(Product p) {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
